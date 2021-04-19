03b465588a86089f9f5b992daaa5aaf99d39f77d
advaitha jayakumar <advaithajayakumar@gmail.com>
2021-04-19 11:36:48 +0530

    bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 int main(){
-    int i,arr[50],size;
+    int i,arr[50],size,temp,j;
     printf("enter the size of array");
     scanf("%d",&size);
     for(i=0;i<size;++i){
         printf("enter the %d element of array",(i+1));
         scanf("%d",&arr[i]);
+    }
+    //bubble sort
+    for(i=0;i<size-1;++i){
+        for(j=0;j<size-1-i;++j){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
     }
     return 0;
 }
